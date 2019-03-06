0a606edf557cb60d2c3c1ac9221a161c0ec1babb
bummings <eabummings@gmail.com>
2019-03-05 22:24:38 -0500

    part one

---
+++ reverseInt.js
@@ -0,0 +1,15 @@
+function reverseInt(num) {
+  let bruh = num.split('');
+  // num.map(number => {
+  //   console.log(number)
+  // })
+  // console.log(num.reverse())
+}
+
+// reverseInt([23, 45, 92])
+
+let bruh = '561';
+let bruhhh = bruh.split(' ');
+if (true) {
+  console.log(bruhhh.reverse().join());
+}
